4809a6fc11cbfcc234a19fd62306041f6bdcd521
Dongwan Kim <zeumes12@gmail.com>
2018-11-26 23:53:05 -0500

    fix version control conflict

--- WhatSAP/Controllers/ActivityController.cs
+++ WhatSAP/Controllers/ActivityController.cs
@@ -90,7 +90,7 @@ namespace WhatSAP.Controllers
             }
 
             var address = _context.Address.FirstOrDefault(x => x.AddressId == activity.AddressId);
-            ViewBag.Address = address.Address2;
+            ViewBag.Address2 = address.Address2;
             ViewBag.Latitude = address.Latitude;
             ViewBag.Longitude = address.Longitude;
             ViewBag.ClientEmail = _context.Client.FirstOrDefault(x => x.ClientId == activity.ClientId).Email;
